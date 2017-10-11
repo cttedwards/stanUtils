6b97efde0b47b0d7e526e23da677e7ae9301974a
Charles <charles.edwards@niwa.co.nz>
2017-10-12 12:19:21 +1300

    change significant figures printed

--- R/utils.R
+++ R/utils.R
@@ -2,12 +2,15 @@
 # Utility functions that are not exported (should not be)
 #
 #  quantile helper function
+#' 
 #' @export
 .quantile <- function(x, dp = 2) {
     
-    y <- round(quantile(x, c(0.5, 0.025, 0.975)), dp)
+    y <- quantile(x, c(0.5, 0.025, 0.975))
     
-    paste0(y[1], " (", y[2], "-", y[3], ")")
+    if(y[1] < 10) z <- signif(y, 3) else z <- round(y, dp)
+    
+    paste0(z[1], " (", z[2], "-", z[3], ")")
 }
 
 #' @export
