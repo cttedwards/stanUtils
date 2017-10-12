bc5782e76b8cecfb059060cf7c0bf069528f7411
Charles <charles.edwards@niwa.co.nz>
2017-10-12 17:55:53 +1300

    sped up flatten function
    and added dimension map to flatten.array

--- R/flatten-stanPosterior.R
+++ R/flatten-stanPosterior.R
@@ -33,29 +33,42 @@
 #' @rdname flatten
 #' @export
 # method
-"flatten.array" <- function(object, par = character()) {
+"flatten.array" <- function(object, par = character(), dim.map = list()) {
     
     # convert array to data.frame for
-    dfr <- plyr::adply(object, 1:length(dim(object)))
-    colnames(dfr)[match("V1", colnames(dfr))] <- 'value'
-    dfr[, 'par'] <- par
+    #dfr <- plyr::adply(object, 1:length(dim(object)))
+    #colnames(dfr)[match("V1", colnames(dfr))] <- 'value'
+    #dfr[, 'par'] <- par
+    
+    # seems faster
+    dfr <- reshape2::melt(object)
+    #dfr[, 'par'] <- par
     
     # create label from parameter name and dimensions
-    dimension.columns <- regexpr("X", colnames(dfr)) > 0
+    dimension.columns <- regexpr("Var", colnames(dfr)) > 0
+    colnames(dfr)[dimension.columns] <- paste0('dim', 1:sum(dimension.columns))
     
-    dfr$label <- apply(dfr, 1, function(x) {
-        dimensions <- x[dimension.columns]
-        if (any(!is.na(dimensions))) {
-            dimensions <- paste(dimensions[!is.na(dimensions)], collapse = ",")
-            label <- paste0(x['par'], "[", dimensions, "]")
-        } else {
-            label <- x['par']
-        }
-        return(label)}
-    )
+    if (length(par) > 0) {
+        dfr$label <- apply(dfr, 1, function(x) {
+            dimensions <- x[dimension.columns]
+            if (any(!is.na(dimensions))) {
+                dimensions <- paste(dimensions[!is.na(dimensions)], collapse = ",")
+                label <- paste0(par, "[", dimensions, "]")
+            } else {
+                label <- par
+            }
+            return(label)}
+        )
+    }
+    
+    if (length(dim.map) > 0) {
+        loc <- which(dimension.columns)
+        for (i in 1:length(loc))
+            dfr[loc[i]] <- names(dim.map[[i]][dfr[,loc[i]]])
+    }
     
     # tidy up
-    dfr <- dfr[, c('iterations', 'value', 'label')]
+    #dfr <- dfr[, c('iterations', 'value', 'label')]
     dfr$iterations <- as.integer(dfr$iterations)
 	
     # return flattened data.frame
@@ -66,9 +79,9 @@
 #' @rdname flatten
 #' @export
 # method
-"flatten.matrix" <- function(object, par = character()) {
+"flatten.matrix" <- function(object, par = character(), dim.map = list()) {
     
-    dfr <- flatten.array(object, par)
+    dfr <- flatten.array(object, par, dim.map)
     
     # return flattened data.frame
     return(dfr)
