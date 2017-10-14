2b91a41b369303b5d7c827cd214e3fc9f6d44681
Charles <charles.edwards@niwa.co.nz>
2017-10-14 13:01:14 +1300

    rebuild

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onAttach <- function(libname, pkgname)
 {
-    packageStartupMessage("stanUtils release version 1.1.0.6 (2017-10-12 17:56:14)")
+    packageStartupMessage("stanUtils release version 1.1.0.10 (2017-10-14 12:45:41)")
 }
